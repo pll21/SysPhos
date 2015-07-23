fe18a7b4b9ec03b4ccb76155872a077f72d1301d
joewledger <joewledger@gmail.com>
2015-07-23 16:16:04 -0400

    Updated SeqConvert process so that the final NetPhorest frame has the original peptide sequences as the index.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- SeqConvert.py
+++ SeqConvert.py
@@ -15,30 +15,27 @@ class SeqConvert:
 		netphorest_frame, sequence_frame = self.get_netphorest_frame()
 
 		print("\tTrimming NetPhorest DataFrame")
-
+		
 		for row in netphorest_frame.iterrows():
-			peptide_num = row[0]
-			peptide_info = sequence_frame.ix[int(peptide_num)]
+			peptide_full_sequence = row[0]
+			peptide_info = sequence_frame.ix[peptide_full_sequence]
 			sites = peptide_info['sites']
 			curr_series = row[1]
 			curr_site = curr_series['Position']
 			if(curr_site in sites):
 				all_series.append(curr_series)
 		netphorest_frame = pd.DataFrame(all_series)
-		sequence_frame.index = [str(x) for x in range(0,len(sequence_frame.index))]
-		netphorest_frame['pval'] = sequence_frame[['pval']]
-		netphorest_frame['fc'] = sequence_frame[['fc']]
+		netphorest_frame['pval'] = sequence_frame['pval']
+		netphorest_frame['fc'] = sequence_frame['fc']
 		return netphorest_frame
 
 	#Returns a data frame containing all the relevant Netphorest Phosphorylation site predictions
 	def get_netphorest_frame(self):
 		sequence_frame = self.get_sequence_frame()
-
 		print("\tGenerating NetPhorest DataFrame")
-
 		self.write_sequence_file(".sequences.fasta",sequence_frame)
 		os.system("cat .sequences.fasta | ./netphorest > .NetPhorest_Predictions.txt")
-		netphorest_frame = pd.DataFrame.from_csv(".NetPhorest_Predictions.txt",sep="\t",index_col=0)
+		netphorest_frame = pd.DataFrame.from_csv(".NetPhorest_Predictions.txt",sep="\t",index_col=0)	
 		cols = netphorest_frame.columns.values
 		renamed_columns = {cols[i - 1]: cols[i] for i in range(1,7)}
 		renamed_columns['Classifier'] = 'Prediction'
@@ -50,53 +47,23 @@ class SeqConvert:
 	#Format of text file that this method expects will be documented later
 	def get_sequence_frame(self):
 		print("\tGenerating Sequence DataFrame")
-
 		df = pd.DataFrame.from_csv(self.filename,sep=" ",index_col=-1)
-		df = df.groupby(df.index).first()
-	
-		indices = list(df.index)
-		sequences, sites = self.strip_sites(indices)
-
-		drop_list = []
-		pop_list = []
-		renamed_index = {}
-		sequence_set = set()
-
-		for i,sequence in enumerate(sequences):
-			if(sequence in sequence_set):
-				drop_list.append(indices[i])
-				pop_list.append(i)
-			else:
-				renamed_index[indices[i]] = sequence
-				sequence_set.add(sequence)
-
-		for i in reversed(pop_list):
-			sites.pop(i)
-
-		df = df.drop(labels=drop_list, axis=0)
-		renamed_index = {indices[i]: sequence for i,sequence in enumerate(sequences)}
-				
-
-		df = df.rename(index=renamed_index)
+		sequences,sites = self.get_sequences_and_sites(df)
+		df['stripped_sequence'] = sequences
 		df['sites'] = sites
-
-		df = df.drop(labels=[label for label in df.index if float(df.ix[label]['pval']) > self.threshold])
-
+		df = df.groupby(df.index,sort=False).first()
 		return df
 
-	#Removes Mass-Spec charge information from sequence strings
-	#Returns a tuple containing:
-	#	1) A list containing all sequence strings without the phosphorylation site data
-	#	2) A list containing the phosphorylation sites for each of the sequnces
-	def strip_sites(self,sequences_with_sites):
+	def get_sequences_and_sites(self,sequence_frame):
 		all_sequences = list()
 		sites = list()
-		for curr_seq in sequences_with_sites:
+		for row in sequence_frame.iterrows():
+			sequence = row[0]
 			build_seq = str()
 			build_sites = list()
 			loc = 0
 			in_brace = False
-			for character in curr_seq:
+			for character in sequence:
 				if(not in_brace and not character == '['):
 					build_seq += character
 					loc += 1
@@ -110,7 +77,18 @@ class SeqConvert:
 			sites.append(build_sites)
 		return all_sequences,sites
 
+
 	def write_sequence_file(self, outfile, sequence_frame):
-		with open(outfile, 'wb') as sequences_file:
-			for index, sequence in enumerate(list(sequence_frame.index)):
-				sequences_file.write(">%s\n%s\n" % (str(index),sequence))
+		sequence_writer = open(outfile,'wb')
+		for row in sequence_frame.iterrows():
+			sequence_with_site = row[0]
+			stripped_sequence = row[1]['stripped_sequence']
+			sequence_writer.write(">%s\n%s\n" % (sequence_with_site,stripped_sequence))
+		sequence_writer.close()
+		
+
+def test():
+	sq = SeqConvert("Testing_Data/test.txt")
+	print(sq.get_trimmed_netphorest_frame())
+
+test()
